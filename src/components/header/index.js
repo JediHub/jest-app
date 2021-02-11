b566779fd7a7e18ba6e0dcddc0eed4d195bc1cfe
jagat <jagat.singh@incedoinc.com>
2021-02-11 15:01:32 +0530

    header setup

---
+++ src/components/header/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './styles.scss'
+
+function Header() {
+    return (
+        <header>
+            <div className='wrap'>
+                <div className='logo'>
+
+                </div>
+            </div>
+       </header>
+    )
+}
+
+export default Header
